465b0dece28339bdb47ec41b6b85e0f3c21ce784
victor <dvicxy@gmail.com>
2021-09-10 09:14:05 +0100

    latest commit

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,4 @@
-import 'package:fcsc_admin/views/scanner.dart';
+
 import 'package:fcsc_admin/views/validation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,108 +13,113 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.transparent,
           elevation: 0.0,
         ),
-        body: Center(
-            child: Column(
-          children: [
-            Image(
-              image: AssetImage('images/logo.png'),
-            ),
-            Container(
-                height: 80,
-                width: 350,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                      decoration: InputDecoration(
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                          backgroundColor: Colors.green,
-                          child: Icon(
-                            Icons.person,
-                            color: Colors.white,
-                          )),
-                    ),
-                    hintText: "Email Address",
-                    hintStyle: GoogleFonts.lato(
-                      fontSize: 14,
-                    ),
-                  )),
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Colors.black12,
+        body: SingleChildScrollView(
+          child: Center(
+              child: Column(
+            children: [
+              Image(
+                image: AssetImage('images/logo.png'),
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                        decoration: InputDecoration(
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            child: Icon(
+                              Icons.person,
+                              color: Colors.white,
+                            )),
+                      ),
+                      hintText: "Email Address",
+                      hintStyle: GoogleFonts.lato(
+                        fontSize: 14,
+                      ),
+                    )),
                   ),
-                )),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.02,
-            ),
-            Container(
-                height: 80,
-                width: 350,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                      decoration: InputDecoration(
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                          backgroundColor: Colors.green,
-                          child: Icon(
-                            Icons.security_outlined,
-                            color: Colors.white,
-                          )),
-                    ),
-                    suffixIcon: Icon(Icons.visibility),
-                    hintText: "Password",
-                    hintStyle: GoogleFonts.lato(
-                      fontSize: 14,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.black12,
                     ),
                   )),
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Colors.black12,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                        decoration: InputDecoration(
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                            backgroundColor: Colors.green,
+                            child: Icon(
+                              Icons.security_outlined,
+                              color: Colors.white,
+                            )),
+                      ),
+                      suffixIcon: Icon(Icons.visibility),
+                      hintText: "Password",
+                      hintStyle: GoogleFonts.lato(
+                        fontSize: 14,
+                      ),
+                    )),
                   ),
-                )),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 60,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ))),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ValidationScreen()),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Sign In', style: TextStyle(color: Colors.white)),
-                      Icon(Icons.arrow_forward, color: Colors.white)
-                    ],
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.black12,
+                    ),
                   )),
-            ),
-          ],
-        )));
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+
+                height: 60,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ))),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ValidationScreen()),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Sign In', style: TextStyle(color: Colors.white)),
+                        Icon(Icons.arrow_forward, color: Colors.white)
+                      ],
+                    )),
+              ),
+            ],
+          )),
+        ));
   }
 }
--- lib/views/validation_screen.dart
+++ lib/views/validation_screen.dart
@@ -9,7 +9,7 @@ class ValidationScreen extends StatefulWidget {
 }
 
 class _ValidationScreenState extends State<ValidationScreen> {
-  String output = 'Unknown';
+  String output = '';
   Future<void> scanQRCode() async {
     try {
       final output = await FlutterBarcodeScanner.scanBarcode(
@@ -42,132 +42,145 @@ class _ValidationScreenState extends State<ValidationScreen> {
               Navigator.pop(context);
             }),
       ),
-      body: SafeArea(
-        child: Container(
-            child: Column(
-          children: [
-            Center(
-              child: Text(
-                "Welcome Back!",
-                style: GoogleFonts.lato(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+              child: Column(
+            children: [
+              Center(
+                child: Text(
+                  "Welcome Back!",
+                  style: GoogleFonts.lato(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            CircleAvatar(
-              radius: 100,
-              backgroundColor: Colors.transparent,
-              backgroundImage: AssetImage('images/logo.png'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              height: 60,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ))),
-                  onPressed: () {
-                    scanQRCode();
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Scan Card',
-                          style: GoogleFonts.lato(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w900,
-                          )),
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Or",
-              style: GoogleFonts.lato(
-                fontSize: 16,
-                color: Colors.black,
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.02,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Validate Exam Number",
-              style: GoogleFonts.lato(
-                fontSize: 20,
-                color: Colors.black,
+              CircleAvatar(
+                radius: 100,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage('images/logo.png'),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-                height: 70,
-                width: 250,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                      decoration: InputDecoration(
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    hintText: "Exam Number",
-                    hintStyle: GoogleFonts.lato(
-                      fontSize: 14,
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.002,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: 60,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.green),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ))),
+                    onPressed: () {
+                      scanQRCode();
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Scan Card',
+                            style: GoogleFonts.lato(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w900,
+                            )),
+                      ],
+                    )),
+              ),
+
+              Center(
+                child: Text(
+                  output,
+                  style: GoogleFonts.lato(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Or",
+                style: GoogleFonts.lato(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Validate Exam Number",
+                style: GoogleFonts.lato(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: 60,
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                        decoration: InputDecoration(
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      hintText: "Exam Number",
+                      hintStyle: GoogleFonts.lato(
+                        fontSize: 14,
+                      ),
+                    )),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.black12,
                     ),
                   )),
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Colors.black12,
-                  ),
-                )),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.6,
-              height: 60,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ))),
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(builder: (context) => ScannerScreen()),
-                    // );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Validate',
-                          style: GoogleFonts.lato(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w900,
-                          )),
-                    ],
-                  )),
-            ),
-          ],
-        )),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                height: 60,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.green),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ))),
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(builder: (context) => ScannerScreen()),
+                      // );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Validate',
+                            style: GoogleFonts.lato(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w900,
+                            )),
+                      ],
+                    )),
+              ),
+            ],
+          )),
+        ),
       ),
     );
   }
