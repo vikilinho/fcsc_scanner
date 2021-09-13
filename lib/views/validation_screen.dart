e5a45fd426978082124e3ce7f813d0fb08f87cb2
victor <dvicxy@gmail.com>
2021-09-13 19:24:52 +0100

    latest commit

--- lib/views/validation_screen.dart
+++ lib/views/validation_screen.dart
@@ -43,6 +43,7 @@ class _ValidationScreenState extends State<ValidationScreen> {
 
       setState(()  {
         this.output = output;
+        //once the code has been scanned, the endpoint is called using the output as parameter
         scanCard();
       });
     } on PlatformException {
@@ -210,6 +211,7 @@ class _ValidationScreenState extends State<ValidationScreen> {
     );
   }
 
+  //The method for validating the card... once user scan the card
   Future<Validation> scanCard() async {
     final prefs = await SharedPreferences.getInstance();
 
@@ -218,153 +220,6 @@ class _ValidationScreenState extends State<ValidationScreen> {
     Map<String, String> headers = {'Authorization': 'Bearer $token'};
     final response = await http.get(endpoint, headers: headers);
     if (response.statusCode == 200) {
-
-      print(response.statusCode);
-      Get.defaultDialog(
-        title: "",
-        content: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.black),
-                      color: Colors.black12),
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child:
-                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 8.0, top: 8.0, bottom: 8.0),
-                          child: Text(
-                              "Victor Ogundola\nAyodeji"
-                          ),
-                        ),
-                        CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.green,
-                          backgroundImage: AssetImage('images/nerc_logo.png'),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.only(
-                                left: 12.0, right: 8.0, bottom: 12.0),
-                            child: Text(
-                              "Exam ID Card",
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
-                              ),
-                            ))
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 5.0),
-                          child: Text(
-                            "Exam No:",
-                            style: GoogleFonts.lato(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            "NERC-05-2021-K46GD-HB612JE9HB",
-                            style: GoogleFonts.lato(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: CircleAvatar(
-                            radius: 70,
-                            backgroundColor: Colors.transparent,
-                            backgroundImage: AssetImage('images/college.jpeg'),
-                          ),
-                        )
-                      ],
-                    ),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(color: Colors.transparent),
-                            color: Colors.white),
-                        height: MediaQuery.of(context).size.height / 2,
-                        width: 370,
-                        child: Column(children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                height: MediaQuery.of(context).size.height / 2,
-                                width: 200,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage('images/barcode.png'),
-                                  ),
-                                  color: Colors.transparent,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "Only Valid For 1 Exam",
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w800,
-                              ),
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.copyright, color: Colors.black, size: 14),
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  "2021 Testmi",
-                                  style: GoogleFonts.lato(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )
-                        ]),
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
-            ),
-          ],
-        )
-      );
       return Validation.fromJson(jsonDecode(response.body));
     } else {
       print(response.statusCode);
