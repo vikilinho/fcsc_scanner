720fb99bad53b35124e6879790cc887de77f7435
victor <dvicxy@gmail.com>
2022-10-26 13:56:04 +0100

    creating new admin page

--- lib/views/admin_page.dart
+++ lib/views/admin_page.dart
@@ -61,6 +61,53 @@ class _AdminPageState extends State<AdminPage> {
                 )
               ],
             ),
+            SizedBox(height: 42.h),
+            Container(
+              height: 200.h,
+              width: 358.w,
+              decoration: BoxDecoration(
+                  color: Color(0xffE6F2EC),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [
+                  Container(
+                    width: 355.w,
+                    height: 150.h,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage("images/container_image.png"))),
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 17.w,
+                      ),
+                      Icon(Icons.qr_code),
+                      SizedBox(
+                        width: 21.25.w,
+                      ),
+                      Text("Scan Exam Card"),
+                      SizedBox(
+                        width: 139.w,
+                      ),
+                      Container(
+                        width: 25.w,
+                        height: 25.h,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 211, 223, 218),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.arrow_forward_rounded,
+                          size: 20,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       )),
