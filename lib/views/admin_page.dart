5a27a93d844dddfa1e9b441b1575bbfac4f1f851
victor <dvicxy@gmail.com>
2022-10-26 13:32:53 +0100

    creating new admin page

--- lib/views/admin_page.dart
+++ lib/views/admin_page.dart
@@ -13,37 +13,56 @@ class _AdminPageState extends State<AdminPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    height: 10.h,
-                    width: 10.w,
-                    decoration: BoxDecoration(color: Colors.red),
-                  )
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hello Admin",
-                    style: TextStyle(fontSize: 14.sp),
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  Text("Welcome Back",
-                      style:
-                          TextStyle(fontSize: 20.sp, color: Color(0xff024126)))
-                ],
-              )
-            ],
-          )
-        ],
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 18.w,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30.h,
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 50.h,
+                      width: 50.w,
+                      decoration: BoxDecoration(
+                          color: Color(0xffF0F3F5),
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      child: Icon(
+                        Icons.person,
+                        color: Color(0xff79807C),
+                        size: 50,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(width: 16.w),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hello, Admin",
+                      style: TextStyle(
+                          fontSize: 14.sp, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    Text("Welcome Back",
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            color: Color(0xff024126),
+                            fontWeight: FontWeight.w700))
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       )),
     );
   }
